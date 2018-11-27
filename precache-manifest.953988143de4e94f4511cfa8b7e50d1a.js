4490ca65e6293a3dcd68cf9f521a69b2a7668df8
Dhrubo Jyoti Paul <dhrubojpaul@gmail.com>
2018-11-27 17:48:31 +0600

    vue cli test

---
+++ precache-manifest.953988143de4e94f4511cfa8b7e50d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0fa8e005cfd97af967b6",
+    "url": "/js/chunk-vendors.3459f54d.js"
+  },
+  {
+    "revision": "ad83cc3f46e999fd6424",
+    "url": "/js/app.c358e551.js"
+  },
+  {
+    "revision": "e6f8897ae0ef8e5387e8",
+    "url": "/js/about.0bf3fb79.js"
+  },
+  {
+    "revision": "0f71fea5c30d6f4c3cc462da5e7ae023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ad83cc3f46e999fd6424",
+    "url": "/css/app.736a4781.css"
+  }
+];
