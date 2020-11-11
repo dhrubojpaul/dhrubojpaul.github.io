f52bc2b52dc490aca64df82b1fe435f284ad1704
dhrubojpaul <dhrubojpaul@gmail.com>
2020-11-11 11:21:23 -0700

    local

---
+++ home/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<p>
+  The Indie (Interactive Data and Immersive Environments) Lab is a research lab within the Department of Computer & Information Science & Engineering (CISE) at the University of Florida. The Indie Lab engages in human-centered research of interactive visualizations. Our research focuses on the design and evaluation of applications and techniques that support effective interaction and understanding of data, information, and virtual environments. Research areas include information visualization, virtual reality, 3D interaction, visual analytics, and educational games.
+</p>
+<p>
+  The Indie Lab includes both undergrad and graduate students from multiple departments from the University of Florida as well as Texas A&M University.
+</p>
+</div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
