111d6db7701dd07c40aeef0f1fe86f36b7f55af1
Dhrubo Jyoti Paul <dhrubojpaul@gmail.com>
2025-08-31 19:46:59 -0400

    Animation changed, mdx addded

---
+++ assets/ms-BlTaqiFL.js
@@ -0,0 +1,8 @@
+import{j as n,P as t,g as i,D as c}from"./index-BTYF0DLc.js";function r(s){const e={h3:"h3",li:"li",p:"p",strong:"strong",ul:"ul",...s.components};return t||o("Projects",!1),t.Links||o("Projects.Links",!0),n.jsxs(n.Fragment,{children:[n.jsx(e.h3,{children:"Key Courses"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Distributed Systems:"})," Learned about fault tolerance and consensus algorithms."]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Machine Learning:"})," Implemented foundational models from scratch."]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Advanced Algorithms:"})," Studied graph theory and dynamic programming."]}),`
+`]}),`
+`,n.jsx(e.p,{children:"Check out the final project for my Distributed Systems course**:"}),`
+`,n.jsx(t.Links,{projects:i(c.MS)})]})}function l(s={}){const{wrapper:e}=s.components||{};return e?n.jsx(e,{...s,children:n.jsx(r,{...s})}):r(s)}function o(s,e){throw new Error("Expected "+(e?"component":"object")+" `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{l as default};
