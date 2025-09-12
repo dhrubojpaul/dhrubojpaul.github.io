1bd32aa8a99574ba0c9655879753cbe84915cf14
Dhrubo Jyoti Paul <dhrubojpaul@gmail.com>
2025-09-11 20:36:40 -0400

    subsection reworked (ongoing)

--- assets/ms-DwIDz80Q.js
+++
@@ -1,8 +0,0 @@
-import{j as e,P as r,g as o,S as i,a as c}from"./index-BIfBz8Tf.js";function t(s){const n={h3:"h3",li:"li",p:"p",strong:"strong",ul:"ul",...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h3,{children:"Key Courses"}),`
-`,e.jsxs(n.ul,{children:[`
-`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Distributed Systems:"})," Learned about fault tolerance and consensus algorithms."]}),`
-`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Machine Learning:"})," Implemented foundational models from scratch."]}),`
-`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Advanced Algorithms:"})," Studied graph theory and dynamic programming."]}),`
-`]}),`
-`,e.jsx(n.p,{children:"Check out the final project for my Distributed Systems course**:"}),`
-`,e.jsx(r,{sectionDetailLevelId:i.HOME,projects:o(c.MS)})]})}function a(s={}){const{wrapper:n}=s.components||{};return n?e.jsx(n,{...s,children:e.jsx(t,{...s})}):t(s)}export{a as default};
---
+++ assets/ms-X4nx4YHL.js
@@ -0,0 +1,8 @@
+import{j as n,P as s,g as i,S as c}from"./index-BerlplaT.js";function o(t){const e={h3:"h3",li:"li",p:"p",strong:"strong",ul:"ul",...t.components};return s||r("Projects",!1),s.Home||r("Projects.Home",!0),n.jsxs(n.Fragment,{children:[n.jsx(e.h3,{children:"Key Courses"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Distributed Systems:"})," Learned about fault tolerance and consensus algorithms."]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Machine Learning:"})," Implemented foundational models from scratch."]}),`
+`,n.jsxs(e.li,{children:[n.jsx(e.strong,{children:"Advanced Algorithms:"})," Studied graph theory and dynamic programming."]}),`
+`]}),`
+`,n.jsx(e.p,{children:"Check out the final project for my Distributed Systems course**:"}),`
+`,n.jsx(s.Home,{subsections:i(c.MS)})]})}function l(t={}){const{wrapper:e}=t.components||{};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}function r(t,e){throw new Error("Expected "+(e?"component":"object")+" `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{l as default};
